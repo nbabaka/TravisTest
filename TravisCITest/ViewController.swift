437a1e766f07e528bdfd071a026a9e37758431b7
Nikolay Karataev <babaka@cinemood.com>
2019-01-06 12:02:14 -0500

    Initial Commit

---
+++ TravisCITest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TravisCITest
+//
+//  Created by Nikolay Karataev on 1/6/19.
+//  Copyright © 2019 Nikolay Karataev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
